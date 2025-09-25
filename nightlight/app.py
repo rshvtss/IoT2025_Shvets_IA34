cc323a4e1bd7bdc8115d96c08d1e8c5cf9bf0e5c
rshvtss <r.shvetsss@gmail.com>
2025-09-25 17:04:58 +0300

    lab 2 changes + zvit + zahist

--- nightlight/app.py
+++ nightlight/app.py
@@ -1,2 +1,11 @@
 from counterfit_connection import CounterFitConnection
+
 CounterFitConnection.init('127.0.0.1', 5000)
+import time
+from counterfit_shims_grove.grove_light_sensor_v1_2 import GroveLightSensor
+
+light_sensor = GroveLightSensor(109)
+while True:
+    light = light_sensor.light
+    print('Light level:', light)
+    time.sleep(1)
